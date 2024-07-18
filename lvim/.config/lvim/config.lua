062725a331bc57bb62ea4fbf8266aefc52e1ab6f
Patrick Laughlin <prlaugh@gmail.com>
2024-07-18 17:08:36 -0400

    tmux

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -3,60 +3,71 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   {
---     name = "stylua",
---     filetypes = { "lua" },
---     args = { "--line-width", "80"}
---   },
--- }
+
 
 lvim.plugins = {
   { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
   {
-    "github/copilot.vim",
-    event = "InsertEnter"
+    "zbirenbaum/copilot-cmp",
+    event = "InsertEnter",
+    config = function() require("copilot_cmp").setup() end,
+    dependencies = {
+      "zbirenbaum/copilot.lua",
+      cmd = "Copilot",
+      config = function()
+        require("copilot").setup({
+          suggestion = { enabled = false },
+          panel = { enabled = false },
+        })
+      end,
+    },
   },
+  -- {
+  --   "github/copilot.vim",
+  --   event = "InsertEnter"
+  -- },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
     dependencies = {
-      { "github/copilot.vim" },    -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+      { "github/copilot.vim" },    -- or github/copilot.vim
     },
     opts = {
-      debug = true, -- Enable debugging
       -- See Configuration section for rest
+      debug = true, -- Enable debugging
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
+  {
+    lazy = false,
+    'akinsho/flutter-tools.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = true,
+  },
 }
 
--- -- Below config is required to prevent copilot overriding Tab with a suggestion
--- -- when you're just trying to indent!
--- local has_words_before = function()
---   if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
---   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
---   return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
--- end
--- local on_tab = vim.schedule_wrap(function(fallback)
---   local cmp = require("cmp")
---   if cmp.visible() and has_words_before() then
---     cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
---   else
---     fallback()
---   end
--- end)
--- lvim.builtin.cmp.mapping["<Tab>"] = on_tab
-
-
+-- vim.g.copilot_no_tab_map = true
+-- vim.g.copilot_assume_mapped = true
+-- vim.g.copilot_tab_fallback = ""
+-- vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+--
 lvim.format_on_save.enabled = true
 lvim.keys.normal_mode["<leader>fr"] = ":FlutterRun<CR>"
 lvim.keys.normal_mode["<leader>fq"] = ":FlutterQuit<CR>"
 lvim.keys.normal_mode["<leader>fR"] = ":FlutterRestart<CR>"
 lvim.keys.normal_mode["<leader>gh"] = ":CopilotChatToggle<CR>"
 
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "dartls" })
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "dartls" })
 lvim.format_on_save.pattern = { "*.dart", "*.lua", "*.ts", "*.js", "*.kt", "*.py", "*.tsx", "*.jsx" }
 lvim.colorscheme = "gruvbox"
+vim.diagnostic.config({
+  virtual_text = false,
+})
+
+vim.cmd [[
+    autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})
+]]
